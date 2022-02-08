fedc548ccd6518bd43f8a297808c2d1407bd506c
Stephen Allard <stephencallard@gmail.com>
2022-02-08 13:03:04 -0600

    add isSunk() method

--- src/ship.js
+++ src/ship.js
@@ -6,4 +6,8 @@ Ship.prototype.hit = function (location) {
   this.damage[location] = 1;
 }
 
+Ship.prototype.isSunk = function() {
+  return this.damage.every((item) => item === 1);
+}
+
 export default Ship;
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -9,12 +9,37 @@ describe('ship constructor', () => {
 });
 
 describe('ship object', () => {
+
   describe('hit()', () => {
     test('when called with integer value representing damage at particular location of the ship, it updates the damage array property', () => {
       const ship = new Ship(4);
+
       ship.hit(3);
 
       expect(ship.damage).toEqual([0, 0, 0, 1]);
+    });
+  });
+
+  describe('isSunk()', () => {
+    test('returns true if each item in the damage array is 1', () => {
+      const ship = new Ship(4);
+
+      ship.hit(0);
+      ship.hit(1);
+      ship.hit(2);
+      ship.hit(3);
+
+      expect(ship.isSunk()).toBe(true);
+    });
+
+    test('returns false if any item in the damage array is not 1', () => {
+      const ship = new Ship(4);
+      
+      ship.hit(0);
+      ship.hit(1);
+      ship.hit(2);
+
+      expect(ship.isSunk()).toBe(false);
     });
   });
 });
