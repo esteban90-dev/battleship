748b9fc02980185d583a378b31aace37d02eca2b
Stephen Allard <stephencallard@gmail.com>
2022-02-17 21:29:08 -0600

    correct ship placements and add turn()

--- src/game.js
+++ src/game.js
@@ -25,18 +25,26 @@ const Game = function(humanPlayer, computerPlayer) {
     computerBoard.placeShip([0, 0], 5, 0);
     computerBoard.placeShip([0, 9], 4, 0);
     computerBoard.placeShip([2, 2], 3, 1);
-    computerBoard.placeShip([9, 7], 3, 1);
-    computerBoard.placeShip([10, 5], 2, 1);
+    computerBoard.placeShip([8, 7], 3, 1);
+    computerBoard.placeShip([9, 5], 2, 1);
 
     // place ships on the human player's board
     humanBoard.placeShip([1, 0], 5, 1);
     humanBoard.placeShip([5, 0], 4, 1);
-    humanBoard.placeShip([0, 10], 3, 0);
+    humanBoard.placeShip([0, 9], 3, 0);
     humanBoard.placeShip([7, 7], 3, 0);
-    humanBoard.placeShip([10, 5], 2, 1);
+    humanBoard.placeShip([9, 5], 2, 1);
   }
 
-  return { initialize }
+  function turn(humanAttackCoordinates) {
+    // send the human's attack to the computer's board
+    computerBoard.receiveAttack(humanAttackCoordinates);
+
+    // send the computer's attack to the human's board
+    humanBoard.receiveAttack(computer.attack());
+  }
+
+  return { initialize, turn }
 
 }
 
